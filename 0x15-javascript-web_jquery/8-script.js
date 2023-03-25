e6fa982247add2259d4c8d3ef822c6882f2a9079
Elijah Lawal Ismaila <104834472+ElijahLawal-7@users.noreply.github.com>
2023-03-25 05:25:00 +0100

    n,

--- 0x15-javascript-web_jquery/8-script.js
+++
@@ -1,8 +0,0 @@
-$('document').ready(() => {
-  $.get('https://swapi-api.hbtn.io/api/films/?format=json',
-    (data) => {
-      $.each(data.results, (index, movie) => {
-        $('UL#list_movies').append('<li>' + movie.title + '</li>');
-      });
-    });
-});
