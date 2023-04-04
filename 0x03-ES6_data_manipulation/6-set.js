8e52f1b81a09155ea4216b04d3e173361d69473e
Ismaila Lawal Elijah <2017en0285@unijos.edu.ng>
2023-04-04 05:16:56 +0100

    data

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Jane Affam <https://github.com/Jayneey>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
